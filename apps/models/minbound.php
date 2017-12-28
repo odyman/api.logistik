4830d6d40bc849c24015b1eded76406715d579b6
odyman <odyodyman@yahoo.com>
2017-12-28 19:18:11 +0700

    inbound save

--- apps/models/minbound.php
+++ apps/models/minbound.php
@@ -197,8 +197,9 @@ class Minbound extends Models {
             // $stmt2->bindParam(1, $return_value, PDO::PARAM_STR, 4000);
             // $stmt2->execute($data);                        
             try {
-              $stmt2 = $this->db()->prepare("CALL `_proses_inbound_bast_barcode_save` (?, ?, ?, ?, ?, ?, ?, ?)");        
-              $stmt2->execute( $data );              
+              $stmt2 = $this->db->prepare("CALL `_proses_inbound_bast_barcode_save` (?, ?, ?, ?, ?, ?, ?, ?)");        
+              $stmt2->execute( $data );                            
+              $this->db->commit();
 
               return true;
             }catch(PDOException $e) {
